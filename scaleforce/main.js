e672ddf3dd8fd1bdeede5fc5e746fc95ca408989
BreadFish64 <mohror64@gmail.com>
2021-07-08 19:03:22 -0500

    Add FXAA prefiltering and URL source

--- scaleforce/main.js
+++ scaleforce/main.js
@@ -76,6 +76,112 @@ void main() {
 }
 `;
 
+
+/**
+Basic FXAA implementation based on the code on geeks3d.com with the
+modification that the texture2DLod stuff was removed since it's
+unsupported by WebGL.
+--
+From:
+https://github.com/mitsuhiko/webgl-meincraft
+Copyright (c) 2011 by Armin Ronacher.
+Some rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * The names of the contributors may not be used to endorse or
+      promote products derived from this software without specific
+      prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+const fxaaFrag = `#version 300 es
+precision mediump float;
+
+in vec2 tex_coord;
+
+out vec4 frag_color;
+
+uniform sampler2D input_texture;
+
+#ifndef FXAA_REDUCE_MIN
+    #define FXAA_REDUCE_MIN   (1.0/ 128.0)
+#endif
+#ifndef FXAA_REDUCE_MUL
+    #define FXAA_REDUCE_MUL   (1.0 / 8.0)
+#endif
+#ifndef FXAA_SPAN_MAX
+    #define FXAA_SPAN_MAX     8.0
+#endif
+
+//optimized version for mobile, where dependent 
+//texture reads can be a bottleneck
+vec4 fxaa(sampler2D tex, vec2 texCoord) {
+    vec4 color;
+    mediump vec2 resolution = vec2(textureSize(tex, 0).xy);
+    mediump vec2 inverseVP = vec2(1.0 / resolution.x, 1.0 / resolution.y);
+    vec3 rgbNW = textureOffset(tex, texCoord, ivec2(-1, -1)).xyz;
+    vec3 rgbNE = textureOffset(tex, texCoord, ivec2(1, -1)).xyz;
+    vec3 rgbSW = textureOffset(tex, texCoord, ivec2(-1, 1)).xyz;
+    vec3 rgbSE = textureOffset(tex, texCoord, ivec2(1, 1)).xyz;
+    vec4 texColor = texture(tex, texCoord);
+    vec3 rgbM  = texColor.xyz;
+    vec3 luma = vec3(0.299, 0.587, 0.114);
+    float lumaNW = dot(rgbNW, luma);
+    float lumaNE = dot(rgbNE, luma);
+    float lumaSW = dot(rgbSW, luma);
+    float lumaSE = dot(rgbSE, luma);
+    float lumaM  = dot(rgbM,  luma);
+    float lumaMin = min(lumaM, min(min(lumaNW, lumaNE), min(lumaSW, lumaSE)));
+    float lumaMax = max(lumaM, max(max(lumaNW, lumaNE), max(lumaSW, lumaSE)));
+    
+    mediump vec2 dir;
+    dir.x = -((lumaNW + lumaNE) - (lumaSW + lumaSE));
+    dir.y =  ((lumaNW + lumaSW) - (lumaNE + lumaSE));
+    
+    float dirReduce = max((lumaNW + lumaNE + lumaSW + lumaSE) *
+                          (0.25 * FXAA_REDUCE_MUL), FXAA_REDUCE_MIN);
+    
+    float rcpDirMin = 1.0 / (min(abs(dir.x), abs(dir.y)) + dirReduce);
+    dir = min(vec2(FXAA_SPAN_MAX, FXAA_SPAN_MAX),
+              max(vec2(-FXAA_SPAN_MAX, -FXAA_SPAN_MAX),
+              dir * rcpDirMin)) * inverseVP;
+    
+    vec3 rgbA = 0.5 * (
+        texture(tex, texCoord + dir * (1.0 / 3.0 - 0.5)).xyz +
+        texture(tex, texCoord + dir * (2.0 / 3.0 - 0.5)).xyz);
+    vec3 rgbB = rgbA * 0.5 + 0.25 * (
+        texture(tex, texCoord + dir * -0.5).xyz +
+        texture(tex, texCoord + dir * 0.5).xyz);
+
+    float lumaB = dot(rgbB, luma);
+    if ((lumaB < lumaMin) || (lumaB > lumaMax))
+        color = vec4(rgbA, texColor.a);
+    else
+        color = vec4(rgbB, texColor.a);
+    return color;
+}
+
+void main() {
+    frag_color = fxaa(input_texture, vec2(tex_coord.x, 1.0 - tex_coord.y));
+}
+`
+
 const scaleforceFrag = `#version 300 es
 precision mediump float;
 
@@ -129,7 +235,7 @@ void main() {
     float total_dist = dot(offset_tl + offset_br, vec4(cross_weight, plus_weight, cross_weight, plus_weight));
 
     if (total_dist == 0.0) {
-        frag_color  = cc;
+        frag_color = cc;
     } else {
         // Add together all the distances with direction taken into account
         vec4 tmp = offset_tl - offset_br;
@@ -169,10 +275,16 @@ function Scaler(gl) {
     this.inputMov = null;
     this.inputWidth = 0;
     this.inputHeight = 0;
+
+    this.useScaleforce = true;
+    this.useFXAA = true;
+
+    this.intermediateFBO = gl.createFramebuffer();
     
-    this.scaleProgram = createProgram(gl, [new Shader(gl, gl.VERTEX_SHADER, quadVert), new Shader(gl, gl.FRAGMENT_SHADER, scaleforceFrag)]);
-    this.bilinearProgram = createProgram(gl, [new Shader(gl, gl.VERTEX_SHADER, quadVert), new Shader(gl, gl.FRAGMENT_SHADER, bilinearFrag)]);
-    gl.useProgram(this.scaleProgram.program);
+    const quadVertShader = new Shader(gl, gl.VERTEX_SHADER, quadVert);
+    this.scaleProgram = createProgram(gl, [quadVertShader, new Shader(gl, gl.FRAGMENT_SHADER, scaleforceFrag)]);
+    this.fxaaProgram = createProgram(gl, [quadVertShader, new Shader(gl, gl.FRAGMENT_SHADER, fxaaFrag)]);
+    this.bilinearProgram = createProgram(gl, [quadVertShader, new Shader(gl, gl.FRAGMENT_SHADER, bilinearFrag)]);
 }
 
 Scaler.prototype.inputImage = function(img) {
@@ -208,7 +320,10 @@ Scaler.prototype.resize = function(scale) {
     gl.canvas.width = width;
     gl.canvas.height = height;
 
-    this.scaleTexture = createTexture(gl, gl.LINEAR, null, width, height);
+    this.intermediateTexture = createTexture(gl, gl.LINEAR, null, this.inputWidth, this.inputHeight);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.intermediateFBO);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.intermediateTexture, 0);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 }
@@ -226,18 +341,31 @@ Scaler.prototype.render = function() {
 
     bindTexture(gl, this.inputTex, 0);
 
+    if (this.useFXAA) {
+        gl.bindFramebuffer(gl.FRAMEBUFFER, this.intermediateFBO);
+        gl.viewport(0, 0, this.inputWidth, this.inputHeight);
+        gl.useProgram(this.fxaaProgram.program);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        bindTexture(gl, this.intermediateTexture, 0);
+    }
+
+    gl.useProgram(this.useScaleforce ? this.scaleProgram.program : this.bilinearProgram.program);
+
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
 
-Scaler.prototype.enableFilter = function(enable) {
-    if (enable) {
-        this.gl.useProgram(this.scaleProgram.program);
-    } else {
-        this.gl.useProgram(this.bilinearProgram.program);
-    }
+Scaler.prototype.enableFXAA = function(enable) {
+    this.useFXAA = enable;
     this.render();
 }
 
+Scaler.prototype.enableFilter = function(enable) {
+    this.useScaleforce = enable;
+    this.render();
+}
 
 let scaler = null;
 
@@ -263,9 +391,18 @@ function onLoad() {
         alert("Can't load the video.");
     }, true);
 
+    // https://www.sitepoint.com/get-url-parameters-with-javascript/
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    sauce = urlParams.get('sauce');
+    if (sauce == null) {
+        sauce = "input.png";
+    }
+    const txtSrc = document.getElementById('txtSrc');
+    txtSrc.value = sauce;
 
     const inputImg = new Image();
-    inputImg.src = "input.png";
+    inputImg.src = sauce;
     inputImg.onload = function() {
         let scale = parseFloat(txtScale.value);
 
@@ -335,6 +472,10 @@ function onSourceChanged() {
     else {
         changeVideo(uri);
     }
+}
+
+function updateFXAA(toggle) {
+    scaler.enableFXAA(toggle.checked);
 }
 
 function updateFilter(toggle) {
